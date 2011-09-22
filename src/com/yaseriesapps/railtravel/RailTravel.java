e690b843d70568cacf5c79aec5509da74668d249
Radha Kallakuri <yasappdev@gmail.com>
2011-09-22 18:33:00 -0500

    Initial Commit for Rail Travel

---
+++ src/com/yaseriesapps/railtravel/RailTravel.java
@@ -0,0 +1,15 @@
+package com.yaseriesapps.railtravel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RailTravel extends Activity 
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
